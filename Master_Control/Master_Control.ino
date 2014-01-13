ace6d5049bf6667592cb2aaf1186a4dd87e54060
Teja Chiluvuri <tejachil@vt.edu>
2014-01-12 23:07:11 -0500

    Fixed the conflict with servo and serial AHRS. Timer code is commented out. Not tested since I am unable to print floats to serial now.

--- Master_Control/Master_Control.ino
+++ Master_Control/Master_Control.ino
@@ -2,6 +2,7 @@
 #include <Servo.h>
 
 const int debugLED = 8;
+volatile boolean stateLED;
 String inputBuf, imuBuf, tempBuf;
 
 enum MOTOR_LOCATIONS {FRONT_LEFT, FRONT_RIGHT, REAR_RIGHT, REAR_LEFT};
@@ -17,123 +18,34 @@ struct Dynamics {
 
 static Dynamics copterDynamics, inputDynamics;
 
-float stringToFloat(String str);
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial1.begin(115200);
-  pinMode(debugLED, OUTPUT);
-  digitalWrite(debugLED, LOW);
-  imuBuf = "";
-  
-  //Servo serv;
-//  serv.attach(8);
-  Serial.println("ARMING MOTORS");
-  //brushlessMotor[FRONT_LEFT].attach(4);
-  /*brushlessMotor[FRONT_RIGHT].attach(5);
-  brushlessMotor[REAR_RIGHT].attach(6);
-  brushlessMotor[REAR_LEFT].attach(7);
-  arm(brushlessMotor[FRONT_LEFT]);
-  arm(brushlessMotor[FRONT_RIGHT]);  
-  arm(brushlessMotor[REAR_RIGHT]);
-  arm(brushlessMotor[REAR_LEFT]);*/
-  delay(500);
-  Serial.println("MOTORS ARMED");
+/*volatile boolean l;
+//TC1 ch 0
+void TC3_Handler()
+{
+        TC_GetStatus(TC1, 0);
+        digitalWrite(8, l = !l);
 }
 
-void loop() {
-  char c = 0;
-
-  while(Serial1.available()){
-//    int bytesAvailable = Serial1.available();
-  //  for(int i = 0; i < bytesAvailable; ++i){
-      c = Serial1.read();
-      if(c == '#')  imuBuf = "";
-      imuBuf += c;
-    //}
-  }
-  
-  if(c == '\n'){
-    imuBuf.trim();
-    Serial.println(imuBuf);
-    copterDynamics.yaw = stringToFloat(imuBuf.substring(imuBuf.indexOf('=')+1, imuBuf.indexOf(',')));
-    tempBuf = imuBuf.substring(imuBuf.indexOf(',')+1);
-    imuBuf = tempBuf;
-    copterDynamics.pitch = stringToFloat(imuBuf);
-    tempBuf = imuBuf.substring(imuBuf.indexOf(',')+1);
-    imuBuf = tempBuf;
-    copterDynamics.roll = stringToFloat(imuBuf);
-
-    /*    
-    Serial.print("\t\t");
-    Serial.print(copterDynamics.yaw);
-    Serial.print('\t');
-    Serial.print(copterDynamics.pitch);
-    Serial.print('\t');
-    Serial.print(copterDynamics.roll);
-    Serial.print('\t');    
-    Serial.println("");
-    */
-  }
-/*
-  inputBuf = "";  
-  while(Serial.available()){
-    c = Serial.read();
-    inputBuf += c;
-    delay(1);
-  }
-  if(!inputBuf.equals("")){
-    if(inputBuf.indexOf("CONTROL DYNAMICS") != -1){
-      digitalWrite(debugLED, HIGH);
-      inputBuf.trim();
-      
-      copterDynamics.yaw = stringToFloat(inputBuf.substring(inputBuf.indexOf('=')+1, inputBuf.indexOf(',')));
-      tempBuf = inputBuf.substring(inputBuf.indexOf(',')+1);
-      inputBuf = tempBuf;
-      copterDynamics.pitch = stringToFloat(inputBuf);
-      tempBuf = inputBuf.substring(inputBuf.indexOf(',')+1);
-      inputBuf = tempBuf;
-      copterDynamics.roll = stringToFloat(inputBuf);
-      if(inputBuf.indexOf("AUTO") == -1){
-        tempBuf = inputBuf.substring(inputBuf.indexOf('::')+2);
-        inputBuf = tempBuf;
-        copterDynamics.throttle = stringToFloat(inputBuf);
-      }
-      else  copterDynamics.throttle = 0.0;
-      
-      /*    
-      Serial.print("\t\t");
-      Serial.print(copterDynamics.yaw);
-      Serial.print('\t');
-      Serial.print(copterDynamics.pitch);
-      Serial.print('\t');
-      Serial.print(copterDynamics.roll);
-      Serial.print('\t');    
-      Serial.print(copterDynamics.throttle);
-      Serial.print('\t');    
-      Serial.println("");
-      */
-      /*digitalWrite(debugLED, LOW);
-    }
-    else{
-      Serial1.print(inputBuf);
-    }
-  }
-  
-  /*setSpeed(brushlessMotor[FRONT_LEFT], (int)copterDynamics.throttle);
-  setSpeed(brushlessMotor[FRONT_RIGHT], (int)copterDynamics.throttle);
-  setSpeed(brushlessMotor[REAR_RIGHT], (int)copterDynamics.throttle);
-  setSpeed(brushlessMotor[REAR_LEFT], (int)copterDynamics.throttle);*/
-    //delay(500);
-//    Serial.println("Hello");
-  //delay(20);
-}
+void startTimer(Tc *tc, uint32_t channel, IRQn_Type irq, uint32_t frequency) {
+        pmc_set_writeprotect(false);
+        pmc_enable_periph_clk((uint32_t)irq);
+        TC_Configure(tc, channel, TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC | TC_CMR_TCCLKS_TIMER_CLOCK4);
+        uint32_t rc = VARIANT_MCK/128/frequency; //128 because we selected TIMER_CLOCK4 above
+        TC_SetRA(tc, channel, rc/2); //50% high, 50% low
+        TC_SetRC(tc, channel, rc);
+        TC_Start(tc, channel);
+        tc->TC_CHANNEL[channel].TC_IER=TC_IER_CPCS;
+        tc->TC_CHANNEL[channel].TC_IDR=~TC_IER_CPCS;
+        NVIC_EnableIRQ(irq);
+}*/
 
 float stringToFloat(String str){
   float ret = str.toInt();
-  if(ret >= 0)  ret += (str.substring(str.indexOf('.') + 1).toInt())/100.0;
-  else  ret -= (str.substring(str.indexOf('.') + 1).toInt())/100.0;
+  String temp = str.substring(str.indexOf('.') + 1);
+  float dec = temp.toInt();
+  dec = dec/100.0;
+  if(ret >= 0)  ret += dec;
+  else  ret -= dec;
   return ret;
 }
 
@@ -150,4 +62,122 @@ void setSpeed(Servo motor, int speed){
   int angle = map(speed, 0, 100, 0, 180);
   motor.write(angle);    
 }
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  pinMode(debugLED, OUTPUT);
+  digitalWrite(debugLED, LOW);
+  imuBuf = "";
+  
+  //Servo serv;
+  //serv.attach(8);
+  Serial.println("ARMING MOTORS");
+  brushlessMotor[FRONT_LEFT].attach(4);
+  brushlessMotor[FRONT_RIGHT].attach(5);
+  brushlessMotor[REAR_RIGHT].attach(6);
+  brushlessMotor[REAR_LEFT].attach(7);
+  arm(brushlessMotor[FRONT_LEFT]);
+  arm(brushlessMotor[FRONT_RIGHT]);  
+  arm(brushlessMotor[REAR_RIGHT]);
+  arm(brushlessMotor[REAR_LEFT]);
+  Serial.println("MOTORS ARMED");
+  //startTimer(TC1, 0, TC3_IRQn, 50); //TC1 channel 0, the IRQ for that channel and the desired frequency
+}
+
+void loop() {
+  char c = 0;
+
+  while(Serial1.available()){
+    c = Serial1.read();
+    if(c == '#')  imuBuf = "";
+    imuBuf += c;
+  }
+  Serial1.flush();
+  if(c == '\n'){
+    imuBuf.trim();
+    Serial.println(imuBuf);
+    
+    tempBuf = imuBuf.substring(imuBuf.indexOf('=')+1);
+    imuBuf = String(tempBuf);
+    copterDynamics.yaw = stringToFloat(imuBuf);
+    tempBuf = imuBuf.substring(imuBuf.indexOf(',')+1);
+    imuBuf = String(tempBuf);
+    copterDynamics.pitch = stringToFloat(imuBuf);
+    tempBuf = imuBuf.substring(imuBuf.indexOf(',')+1);
+    imuBuf = String(tempBuf);
+    copterDynamics.roll = stringToFloat(imuBuf);
+
+    /*Serial.print("\t\t");
+    Serial.print(copterDynamics.yaw);
+    Serial.print("\t");
+    Serial.print(copterDynamics.pitch);
+    Serial.print("\t");
+    Serial.print(copterDynamics.roll);  
+    Serial.println("");*/
+    
+  }
+
+  char d = 0;
+  while(Serial.available()){
+    d = Serial.read();
+    if(d == '$')  imuBuf = "";
+    inputBuf += d;
+  }
+  Serial.flush();
+  if(d == '\n'){
+    digitalWrite(debugLED, stateLED = !stateLED);
+    if(inputBuf.startsWith("$CONTROL DYNAMICS")){
+      inputBuf.trim();
+      
+      tempBuf = inputBuf.substring(inputBuf.indexOf('=')+1);
+      inputBuf = String(tempBuf);
+      inputDynamics.yaw = stringToFloat(inputBuf);
+      tempBuf = inputBuf.substring(inputBuf.indexOf(',')+1);
+      inputBuf = String(tempBuf);
+      inputDynamics.pitch = stringToFloat(inputBuf);
+      tempBuf = inputBuf.substring(inputBuf.indexOf(',')+1);
+      inputBuf = String(tempBuf);
+      inputDynamics.roll = stringToFloat(inputBuf);
+
+      tempBuf = inputBuf.substring(inputBuf.indexOf('::')+2);
+      inputBuf = tempBuf;
+      copterDynamics.throttle = stringToFloat(inputBuf);
+      //if(inputBuf.indexOf("AUTO") != -1)  copterDynamics.throttle = 0.0;
+      /*else{
+        tempBuf = inputBuf.substring(inputBuf.indexOf('::')+2);
+        inputBuf = tempBuf;
+        copterDynamics.throttle = stringToFloat(inputBuf);
+      }*/
+      
+      /*    
+      Serial.print("\t\t");
+      Serial.print(copterDynamics.yaw);
+      Serial.print('\t');
+      Serial.print(copterDynamics.pitch);
+      Serial.print('\t');
+      Serial.print(copterDynamics.roll);
+      Serial.print('\t');    
+      Serial.print(copterDynamics.throttle);
+      Serial.print('\t');    
+      Serial.println("");
+      */
+      //digitalWrite(debugLED, LOW);
+    }
+    /*else{
+      Serial1.print(inputBuf);
+    }*/
+//    digitalWrite(debugLED, LOW);
+  }
+  
+  setSpeed(brushlessMotor[FRONT_LEFT], (int)copterDynamics.throttle);
+  setSpeed(brushlessMotor[FRONT_RIGHT], (int)copterDynamics.throttle);
+  setSpeed(brushlessMotor[REAR_RIGHT], (int)copterDynamics.throttle);
+  setSpeed(brushlessMotor[REAR_LEFT], (int)copterDynamics.throttle);
+    //delay(500);
+//    Serial.println("Hello");
+  //delay(20);
+}
+
 
